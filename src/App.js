79c6e54f1e70e3b7602a13d6c6a471c4ae550443
Nacho Minolli <nachominolli@gmail.com>
2020-10-10 18:23:36 -0300

    ModelMultiMaterial

--- src/App.js
+++ src/App.js
@@ -1,70 +1,74 @@
-import React, { Suspense } from "react";
-import { Canvas, useLoader } from "react-three-fiber";
+import React, { Suspense, useRef } from "react";
+import {
+  Canvas,
+  useLoader,
+  extend,
+  useFrame,
+  useThree,
+} from "react-three-fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import "./styles.css";
 
-function Loading() {
+extend({ OrbitControls });
+
+const Controls = () => {
+  const orbitRef = useRef();
+  const { camera, gl } = useThree();
+
+  useFrame(() => {
+    orbitRef.current.update();
+  });
+
   return (
-    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
-      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
-      <meshStandardMaterial
-        attach="material"
-        color="white"
-        transparent
-        opacity={0.6}
-        roughness={1}
-        metalness={0}
-      />
-    </mesh>
+    <orbitControls
+      enablePan={false}
+      enableZoom={false}
+      autoRotate
+      autoRotateSpeed={45}
+      args={[camera, gl.domElement]}
+      ref={orbitRef}
+    />
   );
-}
+};
 
+function Mesa2() {
+  const gltf = useLoader(GLTFLoader, "mesa/MesaBabylon.glb");
 
-
-function Mesa() {
-  const { nodes } = useLoader(
-    GLTFLoader,
-    "models/MesitaBlackTreadConverter.glb"
-  );
-
-  const scene = useLoader(GLTFLoader, "models/MesitaBlackTreadConverter.glb");
+  const scene = useLoader(GLTFLoader, "mesa/MesaBabylon.glb");
   console.log(scene);
 
   return (
-    <group>
-      <mesh
-        visible
-        geometry={nodes.Mesita.geometry}
-        scale={[0.2, 0.2, 0.2]}
-        rotation={[-Math.PI / 2, 0, 0]}
-      >
-        <meshStandardMaterial
-          attach="material"
-          color="white"
-          roughness={0.3}
-          metalness={0.3}
-        />
-      </mesh>
-    </group>
+    <primitive object={gltf.scene} position={[0, -1, 0]} scale={[7, 7, 7]} />
   );
 }
 
 export default function App() {
   return (
-    <Canvas 
-    style={{ background: "#171717" }}
-    camera={{ position: [0, 2, 4] }}>
-      <ambientLight intensity={0.6} />
-      <spotLight
-        castShadow
-        penumbra={1}
-        position={[15, 4, 12]}
-        color={"white"}
-        intensity={1}
-      />
-      <Suspense fallback={<Loading />}>
-        <Mesa />
-      </Suspense>
-    </Canvas>
+    <>
+      <h2 className="animate__animated animate__flash">
+        MESAS PLUVIAL.
+        <br />
+        <span id="extra">
+        BY FEDERICO CHURBA.
+        </span>
+      </h2>
+
+
+      <Canvas style={{ background: "Peru" }} camera={{ position: [0, 2, 4] }}>
+        <ambientLight intensity={0.6} />
+        <spotLight
+          castShadow
+          penumbra={1}
+          position={[15, 4, 12]}
+          color={"white"}
+          intensity={1}
+        />
+        <Suspense fallback={null}>
+          <Mesa2 />
+        </Suspense>
+        <Controls />
+      </Canvas>
+    </>
   );
 }
