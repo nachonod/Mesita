c16be614b1e1006ce3343b6838a3c8cd93ee109b
Nacho Minolli <nachominolli@gmail.com>
2020-10-10 13:15:02 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Canvas, useLoader } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import "./styles.css";
 
-function App() {
+function Loading() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="white"
+        transparent
+        opacity={0.6}
+        roughness={1}
+        metalness={0}
+      />
+    </mesh>
   );
 }
 
-export default App;
+
+
+function Mesa() {
+  const { nodes } = useLoader(
+    GLTFLoader,
+    "models/MesitaBlackTreadConverter.glb"
+  );
+
+  const scene = useLoader(GLTFLoader, "models/MesitaBlackTreadConverter.glb");
+  console.log(scene);
+
+  return (
+    <group>
+      <mesh
+        visible
+        geometry={nodes.Mesita.geometry}
+        scale={[0.2, 0.2, 0.2]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      >
+        <meshStandardMaterial
+          attach="material"
+          color="white"
+          roughness={0.3}
+          metalness={0.3}
+        />
+      </mesh>
+    </group>
+  );
+}
+
+export default function App() {
+  return (
+    <Canvas 
+    style={{ background: "#171717" }}
+    camera={{ position: [0, 2, 4] }}>
+      <ambientLight intensity={0.6} />
+      <spotLight
+        castShadow
+        penumbra={1}
+        position={[15, 4, 12]}
+        color={"white"}
+        intensity={1}
+      />
+      <Suspense fallback={<Loading />}>
+        <Mesa />
+      </Suspense>
+    </Canvas>
+  );
+}
